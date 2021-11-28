fed37c871f70c1368be343f647c286cafbe5ab43
Dhruv <dev.aroradhruv@gmail.com>
2021-11-28 08:43:23 +0530

    apiKey

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 export default class App extends Component {
   pageSize = 9;
+  apiKey = process.env.REACT_APP_NEWS_API;
   render() {
     return (
       <div>
@@ -15,6 +16,7 @@ export default class App extends Component {
           <Switch>
             <Route exact path="/">
               <News
+                apiKey={this.apiKey}
                 key="general"
                 pageSize={this.pageSize}
                 country="in"
@@ -23,6 +25,7 @@ export default class App extends Component {
             </Route>
             <Route exact path="/business">
               <News
+                apiKey={this.apiKey}
                 key="business"
                 pageSize={this.pageSize}
                 country="in"
@@ -31,6 +34,7 @@ export default class App extends Component {
             </Route>
             <Route exact path="/entertainment">
               <News
+                apiKey={this.apiKey}
                 key="entertainment"
                 pageSize={this.pageSize}
                 country="in"
@@ -39,6 +43,7 @@ export default class App extends Component {
             </Route>
             <Route exact path="/general">
               <News
+                apiKey={this.apiKey}
                 key="general"
                 pageSize={this.pageSize}
                 country="in"
@@ -46,10 +51,17 @@ export default class App extends Component {
               />
             </Route>
             <Route exact path="/health">
-              <News key="health" pageSize={this.pageSize} country="in" category="health" />
+              <News
+                apiKey={this.apiKey}
+                key="health"
+                pageSize={this.pageSize}
+                country="in"
+                category="health"
+              />
             </Route>
             <Route exact path="/science">
               <News
+                apiKey={this.apiKey}
                 key="science"
                 pageSize={this.pageSize}
                 country="in"
@@ -57,10 +69,17 @@ export default class App extends Component {
               />
             </Route>
             <Route exact path="/sports">
-              <News key="sports" pageSize={this.pageSize} country="in" category="sports" />
+              <News
+                apiKey={this.apiKey}
+                key="sports"
+                pageSize={this.pageSize}
+                country="in"
+                category="sports"
+              />
             </Route>
             <Route exact path="/technology">
               <News
+                apiKey={this.apiKey}
                 key="technology"
                 pageSize={this.pageSize}
                 country="in"
--- src/Components/News.js
+++ src/Components/News.js
@@ -30,7 +30,7 @@ export class News extends Component {
   }
 
   async updateNews(pageNo) {
-    const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=8a4aaac048ce4d1ea4a7354982de5fdf&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+    const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.props.apiKey}&page=${this.state.page}&pageSize=${this.props.pageSize}`;
     this.setState({ loading: true });
     let data = await fetch(url);
     let parsedData = await data.json();
